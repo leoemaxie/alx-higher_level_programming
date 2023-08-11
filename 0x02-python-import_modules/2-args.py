0af9a5b9b0c9f0c248f8a0462c0b4a5a7326e6fb
leoemaxie <leoemaxie@gmail.com>
2023-08-11 07:08:39 +0100

    Bug fixes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    """ Performs basic arithmetic calculations on two numbers """
+    import sys
 
-# Excludes the name of tge program
-args_len = len(sys.argv) - 1
-msg = "argument" if args_len == 1 else "arguments"
-print("{} {}".format(args_len, msg), end=":\n" if args_len > 0 else ".\n")
+    # Excludes the name of this program
+    args_len = len(sys.argv) - 1
+    msg = "argument" if args_len == 1 else "arguments"
+    print("{} {}".format(args_len, msg), end=":\n" if args_len > 0 else ".\n")
 
-for i in range(1, args_len + 1):
-    print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, args_len + 1):
+        print("{}: {}".format(i, sys.argv[i]))
 
-sys.exit(0)
+    sys.exit(0)
+
