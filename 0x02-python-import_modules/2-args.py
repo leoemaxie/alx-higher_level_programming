2fe89a8d314aa805618b762c5355aa4dc34ae664
leoemaxie <leoemaxie@gmail.com>
2023-08-12 18:52:35 +0100

    ...

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,14 +5,14 @@ if __name__ == "__main__":
 
     # Excludes the name of this program
     count = len(sys.argv) - 1
-     if count == 0:
-         print("0 arguments.")
-     elif count == 1:
-         print("1 argument:")
-     else:
-         print("{} arguments:".format(count))
-     for i in range(count):
-         print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+        for i in range(count):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
 
     sys.exit(0)
 
