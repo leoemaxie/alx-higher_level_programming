6b03c02dfcd1b886ca82f5ceb5c62567b918de52
Emmanuel Lafenwa <103007301+leoemaxie@users.noreply.github.com>
2023-08-14 22:54:40 +0100

    Fixed bug and optimized 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     """Prints a matrix of integers."""
     for array in matrix:
         for elem in array:
-            print("{:d}".format(elem), end=" " if elem != elem[-1] else "")
+            print("{:d}".format(elem), end=" " if elem != array[-1] else "")
         print()
