626abba0f2b1ab737e035e83f890595326b5e195
leoemaxie <leoemaxie@gmail.com>
2023-08-16 18:42:57 +0100

    Optimized and removed dead/unnecessary code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     """Returns a key with the biggest integer value."""
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
 
-    max = 0
-    max_key = ""
+    max = 0, max_key = ""
     for key, value in a_dictionary.items():
         if value > max:
             max_key = key
