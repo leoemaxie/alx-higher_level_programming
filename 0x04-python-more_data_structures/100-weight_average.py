a57266a2ba7eebeac6e4784f3714ab068b31f346
leoemaxie <leoemaxie@gmail.com>
2023-08-16 19:54:45 +0100

    Bug Fixes

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    """Returns the weighted average of all integers tuple (<score>, <weight>)."""
-    weight, divisor, multiplier  = (0, 0, 1)
+    """Returns the weighted average of all integers tuple (score, weight)"""
+    weight, divisor, multiplier = (0, 0, 1)
 
     if not my_list:
         return 0
--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, del_value):
     """Deletes keys with a specific value in a dictionary."""
+    if not a_dictionary:
+        return None
     for key, value in a_dictionary.items():
         if value == del_value:
             del a_dictionary[key]
