e501b44f3065a372e9d46da1955364c4551f6134
leoemaxie <leoemaxie@gmail.com>
2023-08-29 05:57:19 +0100

    Fixed some handling errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     total = 0
     for i in range(x):
         try:
-            total += 1
             print("{}".format(my_list[i]), end="")
+            total += 1
         except IndexError:
             break
     print()
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,14 +3,13 @@ def safe_print_list_integers(my_list=[], x=0):
     """Prints the first x elements of a list and only integers."""
     total = 0
 
-    try:
-        for i in range(x):
-            if type(my_list[i]).instanceof(int):
-                total += 1
-                print("{:d}".format(my_list[i]), end="")
-        print()
-        return total
-    except IndexError as e:
-        return total
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            total += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return total
 
 
