0366ff7adb3799ba0b84f2179d04c15898903460
leoemaxie <leoemaxie@gmail.com>
2023-08-30 23:20:22 +0100

    Square Class completed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Square Moduule: Defines a Square class with a private variable size"""
+"""Square Moduule: Defines a Square class with a private variable {size}"""
 
 
 class Square:
-    """An empty square class"""
+    """A square class with a private instance variable"""
     def __init__(self, size):
         self.__size = size
