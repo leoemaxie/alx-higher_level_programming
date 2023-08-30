9aaa72a6229435f3da88b541d5f18ad617258cad
leoemaxie <leoemaxie@gmail.com>
2023-08-30 21:55:45 +0100

    Updated square class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Square Moduule: Defines a Square class"""
+
+
 class Square:
+    """An empty square class"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Square Moduule: Defines a Square class with a private variable size"""
+
+
 class Square:
+    """An empty square class"""
     def __init__(self, size):
-        self,__size = size
+        self.__size = size
