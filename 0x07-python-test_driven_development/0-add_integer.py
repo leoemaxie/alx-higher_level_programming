df2989eeb0a40cb9245378237a1c6a92a866e772
leoemaxie <leoemaxie@gmail.com>
2023-09-02 04:12:14 +0100

    Added 0-add_integer.py and its test suites

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
-"""Square Moduule: Defines a Square class"""
+"""Addition Moduule: Defines an addition {add_integer} function"""
 
 
-class Square:
-    """An empty square class"""
-    pass
+def add_integer(a, b=98):
+    """Adds two integers
+
+       Return: addition of a and b
+    """
+    if isinstance(a, bool) or isinstance(b, bool):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(a, int):
+        if isinstance(a, float):
+            a = int(a)
+        else:
+            raise TypeError("a must be an integer")
+
+    if not isinstance(b, int):
+        if isinstance(b, float):
+            b = int(b)
+        else:
+            raise TypeError("b must be an integer")
+
+    return a + b
